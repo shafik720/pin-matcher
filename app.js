9e6614b6f0e1e8bb09d8a584409e990be3139a20
shafik720 <shafikrasel5@gmail.com>
2022-08-08 19:26:46 +0600

    sxdfsdf

--- app.js
+++ app.js
@@ -21,11 +21,26 @@ document.getElementById('calcBody').addEventListener('click',function(event){
         if(number == 'C'){
             display.value = '';
         }
-    }else{
+    }else if((display.value).length==4){
+
+    }
+    else{
     let previousNumber = display.value;
     let currentNumber = previousNumber + number;
-    display.value = currentNumber;
-    console.log(display.value);
+    display.value = currentNumber;    
+    }    
+})
+
+document.getElementById('submit').addEventListener('click', function(){
+    let pinDisplay = document.getElementById('pinDisplay').value;
+    let matchDisplay = document.getElementById('display').value;
+    let errorMsg = document.getElementById('error');
+    let success = document.getElementById('success');
+    if(pinDisplay == matchDisplay){
+        success.style.display = 'block'
+        errorMsg.style.display = 'none';
+    }else{
+        errorMsg.style.display = 'block';
+        success.style.display = 'none'
     }
-    
 })
